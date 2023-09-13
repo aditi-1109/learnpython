52ca59cb5796bff823f37b2109744335252ddf50
aditi-1109 <adii.9112.ar@gmail.com>
2023-09-13 11:53:23 +0530

    init functionality implemented

--- learn.py
+++ learn.py
@@ -1,7 +1,18 @@
-class A:
-    x = 10
-    def display(self):
-        print(self.x)
+# class A:
+#     x = 10
+#     def display(self):
+#         print(self.x)
 
-obj = A()
-obj.display()
+# obj = A()
+# obj.display()
+
+class A:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def display(self):
+        print(self.name," ",self.age)
+
+B = A("Aditi",24)
+B.display()
